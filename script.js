b5be6a06f5c9220ac600e76610b47c511c10ccf2
calebe1234 <142435578+calebe1234@users.noreply.github.com>
2024-04-30 11:33:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+class objeto{
+    constructor (parametro1,parametro2 ){
+        this.parametro1 = parametro1;
+        this.parametro2 = parametro2;
+    }
+    setodo1 (){
+     window
+    }
+    setodo2() {
+
+    }
+}
+class Calebe{
+    constructor(site){
+        this.site = site;
+
+
+    }
+}
